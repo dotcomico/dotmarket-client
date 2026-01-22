3ff3448782eb2e5c1ffd4084e6a688cd2c9bd351
dotan <dotcompanyofficial@gmail.com>
2026-01-23 00:35:06 +0200

    feat: export profile components

---
+++ src/features/profile/index.ts
@@ -0,0 +1,3 @@
+export { ProfileHeader } from './components/ProfileHeader/ProfileHeader';
+export { ProfileSettings } from './components/ProfileSettings/ProfileSettings';
+export { AccountActions } from './components/AccountActions/AccountActions';
