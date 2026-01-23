abe8afe2306a8ca9a169f5c6a85894b967f2df5c
dotan <dotcompanyofficial@gmail.com>
2026-01-24 01:25:12 +0200

    create index types for the global project types

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Global Types Index
+ * Re-exports all shared types for convenient imports
+ * 
+ * Usage: import { User, Pagination, LoadingState } from '@/types';
+ */
+
+export * from './user.types';
+export * from './api.types';
+export * from './common.types';i
+
