f6e6a09b0bfee871ae1e8269912a4678d5322091
dotan <dotcompanyofficial@gmail.com>
2026-01-24 01:24:40 +0200

    apdated file name for import cart iten,cartstate

--- src/features/cart/index.ts
+++ src/features/cart/index.ts
@@ -2,4 +2,4 @@ export { CartItemCard } from './components/CartItemCard/CartItemCard';
 export { CartSummary } from './components/CartSummary/CartSummary';
 export { EmptyCart } from './components/EmptyCart/EmptyCart';
 export { useCart } from './hooks/useCart';
-export type { CartItem, CartState } from './types/cart.type';
+export type { CartItem, CartState } from './types/cart.types';
