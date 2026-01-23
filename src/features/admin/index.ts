45ebfdd407d5f11df8984146d5f294d6b16e039a
dotan <dotcompanyofficial@gmail.com>
2026-01-24 00:33:31 +0200

    Add index.ts for clean imports

---
+++ src/features/admin/index.ts
@@ -0,0 +1,11 @@
+// Dashboard Components
+export { DashboardStats } from './components/DashboardStats/DashboardStats';
+export { RecentOrdersTable } from './components/RecentOrdersTable/RecentOrdersTable';
+export { LowStockAlert } from './components/LowStockAlert/LowStockAlert';
+export { QuickStatsGrid } from './components/QuickStatsGrid/QuickStatsGrid';
+
+// Hooks
+export { useAdminAccess } from './hooks/useAdminAccess';
+
+// Types
+export type { DashboardStats as DashboardStatsType } from './types/admin.types';
