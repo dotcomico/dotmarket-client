5feb1939a0ea2d7dbe164905c597d46d39d66e2b
dotan <dotcompanyofficial@gmail.com>
2026-01-26 20:22:00 +0200

    fix: remove stray character causing syntax error

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,8 @@
 /**
  * Global Types Index
  * Re-exports all shared types for convenient imports
- * 
- * Usage: import { User, Pagination, LoadingState } from '@/types';
  */
 
 export * from './user.types';
 export * from './api.types';
-export * from './common.types';i
-
+export * from './common.types';
