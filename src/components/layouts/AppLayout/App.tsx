549c6757d838ca794404b72cd6e0f4f3982517de
dotan <dotcompanyofficial@gmail.com>
2026-01-27 02:41:35 +0200

    fix(layout): hide client header/footer on admin routes

--- src/components/layouts/AppLayout/App.tsx
+++ src/components/layouts/AppLayout/App.tsx
@@ -1,17 +1,35 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import "./App.css";
 import Fotter from "./Fotter/Fotter";
 import AppRoutes from "../../../routes/AppRoutes";
 import Header from "./Header/Header";
 
+/**
+ * Inner component that has access to router context
+ * Conditionally renders Header/Footer based on current route
+ */
+const AppContent = () => {
+  const location = useLocation();
+  
+  // Check if current route is an admin route
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
+  return (
+    <>
+      {/* Only render client Header/Footer on non-admin routes */}
+      {!isAdminRoute && <Header />}
+      <AppRoutes />
+      {!isAdminRoute && <Fotter />}
+    </>
+  );
+};
+
 export default function App() {
-return (
-        <div className="app">
-            <BrowserRouter>
-            <Header/>
-            <AppRoutes/>
-            <Fotter/>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </div>
+  );
 }
