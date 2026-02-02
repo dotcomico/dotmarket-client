8c9b4080d377cbc8c6b84bcb6e89392b1ff9039b
dotan <dotcompanyofficial@gmail.com>
2026-02-02 19:14:39 +0200

    Add product grid to view

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from "react";
 import "./Home.css";
 import { CategoryList } from '../../features/categories';
+import { ProductGrid, useProductStore } from "../../features/products";
 
 const Home = () => {
+  const { products, isLoading, fetchProducts } = useProductStore();
+
+  useEffect(() => {
+    if (products.length === 0) {
+      fetchProducts({ limit:1  });
+    }
+  }, [products.length, fetchProducts]);
+
   return (
     <div className='Home'>
-    
-    
       <CategoryList variant="grid" limit={9} showChildren={false} />
-      
+      <ProductGrid products={products} isLoading={isLoading} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
